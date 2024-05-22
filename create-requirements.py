bb504f90f8d8c0dcef3c2f7d530779a1626e67dc
Ben Lewis <blewis@hirundo.io>
2024-05-22 23:36:59 +0300

    Create `dev-requirements.txt` via updates to `create-requirements.txt` and update `pytest.yml` GitHub Actions workflow

--- create-requirements.py
+++ create-requirements.py
@@ -1,17 +1,25 @@
 import yaml
 
-with open("environment.yml", "r") as f:
-    try:
-        dependencies: list[dict] = yaml.safe_load(f)["dependencies"]
-        for dep in dependencies:
-            if isinstance(dep, dict) and dep.get("pip"):
-                pip_requirements = [
-                    pip_dep for pip_dep in dep["pip"] if not pip_dep.startswith("-e")
-                ]
-                break
-    except yaml.YAMLError as exc:
-        print(exc)
-        raise Exception("Failed to parse environment.yaml") from exc
+def get_requirements(file_name: str):
+    with open(file_name, "r") as f:
+        try:
+            dependencies: list[dict] = yaml.safe_load(f)["dependencies"]
+            for dep in dependencies:
+                if isinstance(dep, dict) and dep.get("pip"):
+                    return [
+                        pip_dep for pip_dep in dep["pip"] if not pip_dep.startswith("-e")
+                    ]
+                    break
+        except yaml.YAMLError as exc:
+            print(exc)
+            raise Exception("Failed to parse environment.yaml") from exc
+
+pip_requirements = get_requirements("environment.yml")
+pip_dev_requirements = get_requirements("dev-environment.yml")
 
 with open("requirements.txt", "w") as f:
     f.write("\n".join(pip_requirements))
+
+with open("dev-requirements.txt", "w") as f:
+    f.write("\n".join(pip_requirements) + "\n")
+    f.write("\n".join(pip_dev_requirements) + "\n")
