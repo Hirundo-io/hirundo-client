345a7b8df1a976e8034f694795a8f7760c5556c3
Ben Lewis <blewis@hirundo.io>
2024-09-03 14:20:47 +0300

    Drop check which is likely to break docs

--- hirundo/_env.py
+++ hirundo/_env.py
@@ -14,11 +14,6 @@ if os.path.exists(EnvLocation.DOTENV.value):
     load_dotenv(EnvLocation.DOTENV.value)
 elif os.path.exists(EnvLocation.HOME.value):
     load_dotenv(EnvLocation.HOME.value)
-else:
-    if not os.getenv("API_KEY") and not os.getenv("API_HOST"):
-        raise ValueError(
-            f"No {EnvLocation.HOME.value} or {EnvLocation.DOTENV.value} files found and API_KEY and API_HOST environment variables are not set"
-        )
 
 API_HOST = os.getenv("API_HOST", "https://api.hirundo.io")
 API_KEY = os.getenv("API_KEY")
