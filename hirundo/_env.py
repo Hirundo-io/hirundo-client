ce2ec563ef297f673122acec3aa15ab9c2a917de
Ben Lewis <blewis@hirundo.io>
2024-09-03 14:11:36 +0300

    Fix error when using environment variables instead of `.hirundo.conf` or `.env`

--- hirundo/_env.py
+++ hirundo/_env.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 class EnvLocation(enum.Enum):
     DOTENV = Path.cwd() / ".env"
-    HOME = Path.home() / "hirundo.conf"
+    HOME = Path.home() / ".hirundo.conf"
 
 
 if os.path.exists(EnvLocation.DOTENV.value):
@@ -15,7 +15,10 @@ if os.path.exists(EnvLocation.DOTENV.value):
 elif os.path.exists(EnvLocation.HOME.value):
     load_dotenv(EnvLocation.HOME.value)
 else:
-    raise ValueError("No .env file found")
+    if not os.getenv("API_KEY") and not os.getenv("API_HOST"):
+        raise ValueError(
+            f"No {EnvLocation.HOME.value} or {EnvLocation.DOTENV.value} files found and API_KEY and API_HOST environment variables are not set"
+        )
 
 API_HOST = os.getenv("API_HOST", "https://api.hirundo.io")
 API_KEY = os.getenv("API_KEY")
